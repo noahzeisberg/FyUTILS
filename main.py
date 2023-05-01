f6b9307da83d958a14465ccfc9b0042a202e4846
NoahOnFyre <noahonfyre@gmail.com>
2023-05-01 23:09:37 +0200

    Fixed version identifier.

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ try:
     print(prefix("INIT") + "Start time: " + str(start_time))
     current_dir = sys.path[0]
     print(prefix("INIT") + "Directory: " + current_dir)
-    version = "1.5.2"
+    version = "1.5.3"
     print(prefix("INIT") + "Version: " + version)
     threads = multiprocessing.cpu_count()
     print(prefix("INIT") + "ThreadWorkers: " + str(threads))
