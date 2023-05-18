ea5bc2dd87801efa588b34b79d70555f082f74e6
NoahOnFyre <noahonfyre@gmail.com>
2023-05-18 21:42:20 +0200

    Updated version identifier.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from pypresence import Presence
 from pytube import YouTube
 
 init(convert=True)
-CURRENT_FYUTILS_VERSION = "1.8.2"
+CURRENT_FYUTILS_VERSION = "1.8.1"
 SUPPORTED_FUEL_VERSION = 1
 
 
