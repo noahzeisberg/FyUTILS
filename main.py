75490db75819f1eb6600f27aee0f8dbc57dc7919
NoahOnFyre <noahonfyre@gmail.com>
2023-05-20 22:15:44 +0200

    Added RPC to restart command.

--- main.py
+++ main.py
@@ -1204,6 +1204,7 @@ try:
                 menu()
 
             case "restart" | "rs":
+                update_status("Restarting...")
                 print("logout")
                 execute("start " + current_dir + "\\main.py")
                 print("login")
