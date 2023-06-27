66e90851b68200750924d4166654b044cd1f46f8
NoahOnFyre <noahonfyre@gmail.com>
2023-06-27 22:04:43 +0200

    Added "get_threaded_function" converter.

--- async_tool_collection.py
+++ async_tool_collection.py
@@ -1,6 +1,8 @@
 # All functions here will be later added to the main.py/async.py. This is just because I want the main.py to be clean until the release of the async stuff.
 # These functions are providing faster coding, so it's better for me and other developers. If you want to use something in your project, please mention me. :D
 
+import asyncio
+
 
 def get_threaded_function(func, /):
-    print()
+    return asyncio.to_thread(func)
