7091caf13c6f1a8450c6b02635d4bb28d1e41f6d
NoahOnFyre <noahonfyre@gmail.com>
2023-06-29 14:04:32 +0200

    Added while loop to run_async().

--- async.py
+++ async.py
@@ -2,28 +2,32 @@
 
 import asyncio
 import datetime
+import time
 
 from requests_futures.sessions import FuturesSession
+import requests_futures
 
 
 def run_async():
-    print("started!")
-    session = FuturesSession()
-    releases_json = session.get("https://api.github.com/repos/NoahOnFyre/FyUTILS/releases").result().json()
-    newest_release = releases_json[0]
-    for r in range(len(newest_release["assets"])):
-        release_download_url = ""
-        if newest_release["assets"][r]["name"] == "main.py":
-            release_download_url = newest_release["assets"][r]["browser_download_url"]
-            break
-        else:
+    while True:
+        print("started!")
+        session = FuturesSession()
+        releases_json = session.get("https://api.github.com/repos/NoahOnFyre/FyUTILS/releases").result().json()
+        newest_release = releases_json[0]
+        for r in range(len(newest_release["assets"])):
             release_download_url = ""
-            continue
-    newest_version_note = newest_release["body"]
-    newest_version = newest_release["tag_name"]
-    print(newest_version_note)
-    print(newest_version)
-    print("done!")
+            if newest_release["assets"][r]["name"] == "main.py":
+                release_download_url = newest_release["assets"][r]["browser_download_url"]
+                break
+            else:
+                release_download_url = ""
+                continue
+        newest_version_note = newest_release["body"]
+        newest_version = newest_release["tag_name"]
+        print(newest_version_note)
+        print(newest_version)
+        print("done!")
+        time.sleep(3)
 
 
 async def main():
