690389b9e09696920069eac65152a8677cbf2a77
NoahOnFyre <noahonfyre@gmail.com>
2023-10-23 20:27:57 +0200

    Add logging to main.go.

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/NoahOnFyre/gengine/color"
+	"github.com/NoahOnFyre/gengine/logging"
+)
 
+func main() {
+	logging.SetMainColor(color.BlueBg)
+	logging.Log("")
 }
