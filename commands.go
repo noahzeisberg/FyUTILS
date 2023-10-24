630bfe6c6eeb224158a51986a3dd77a278895ec0
NoahOnFyre <noahonfyre@gmail.com>
2023-10-24 19:19:39 +0200

    Fix port scanner.

--- commands.go
+++ commands.go
@@ -51,16 +51,7 @@ func PortscanCommand(args []string) {
 
 	for port := 1; port <= 65535; port++ {
 		wg.Add(1)
-		go func(target string, port int, wg *sync.WaitGroup) {
-			defer wg.Done()
-			address := net.JoinHostPort(target, convert.FormatInt(port))
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			defer conn.Close()
-			logging.Log("Port", port, "is open!")
-		}(addr, port, &wg)
+		go ScanPort(addr, port, &wg)
 	}
 	wg.Wait()
 
