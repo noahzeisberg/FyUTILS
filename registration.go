23d5fd2ff9f269ecbf91ba421c94b06e9b64d9a1
NoahOnFyre <noahonfyre@gmail.com>
2023-10-24 19:42:14 +0200

    Add missing argument to "portscan" command in registration.go

--- registration.go
+++ registration.go
@@ -7,7 +7,7 @@ import (
 
 func CommandRegistration() {
 	RegisterCommand("flood", "Run a denial of service attack on the target.", []string{"addr", "port"}, FloodCommand)
-	RegisterCommand("portscan", "Scan for open ports on the target.", []string{"addr"}, PortscanCommand)
+	RegisterCommand("portscan", "Scan for open ports on the target.", []string{"addr", "max-port"}, PortscanCommand)
 	RegisterCommand("gather", "Gather information about the target.", []string{"addr"}, GatherCommand)
 	// RegisterCommand("update", "Update your FyUTILS instance.", []string{}, UpdateCommand)
 	RegisterCommand("help", "Show some help about the commands.", []string{}, HelpCommand)
