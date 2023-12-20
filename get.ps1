72da5beace0aab12b2a6aeb2c7e042aeb17b93e9
NoahOnFyre <noahonfyre@gmail.com>
2023-12-20 20:55:07 +0100

    Move get.ps1 from vulcan (#59)

---
+++ get.ps1
@@ -0,0 +1,52 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "You need to have PowerShell running as administrator."
+    Write-Error "Please exit the installation and try to run again as administrator."
+    $null = $host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    exit
+}
+
+Write-Output "Starting FyUTILS installation..."
+Write-Output "Setting up variables..."
+$fileName = "FyUTILS.exe"
+$folderPath = Join-Path $env:USERPROFILE ".fy"
+$filePath = Join-Path $folderPath $fileName
+$githubApiUrl = "https://api.github.com/repos/noahonfyre/FyUTILS/releases/latest"
+
+Write-Output "Creating local file..."
+New-Item -ItemType Directory -Path $folderPath -Force | Out-Null
+
+Write-Output "Fetching API information..."
+$releaseInfo = Invoke-RestMethod -Uri $githubApiUrl
+
+Write-Output "Processing data..."
+$fileDownloadUrl = $releaseInfo.assets | Where-Object { $_.name -eq $fileName } | Select-Object -ExpandProperty browser_download_url
+
+Write-Output "Downloading file from GitHub..."
+Invoke-WebRequest -Uri $fileDownloadUrl -OutFile $filePath
+
+Write-Output "Checking for environment variable..."
+if (-not ($folderPath -in $env:FyUTILS)) {
+    Write-Output "Adding directory to your environment variables..."
+    [Environment]::SetEnvironmentVariable("FyUTILS", $filePath, "Machine")
+}
+
+Write-Output "Excluding FyUTILS directory from Windows Defender..."
+if (Get-Command -ErrorAction SilentlyContinue Get-MpPreference) {
+    $existingExclusions = Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
+    if ($existingExclusions -contains $folderPath) {
+        Write-Output "Exclusion for $folderPath already exists. No changes made."
+    }
+    else {
+        $existingExclusions += $folderPath
+        Set-MpPreference -ExclusionPath $existingExclusions
+        Write-Output "Exclusion for $folderPath added successfully."
+    }
+}
+else {
+    Write-Warning "Windows Defender is not installed or not available on this system."
+}
+
+Write-Output " "
+Write-Output "Installation of FyUTILS complete! Please consider starring this repository."
+Write-Output "https://github.com/noahonfyre/FyUTILS"
+Write-Output " "
