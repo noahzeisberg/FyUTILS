08051686827e6dc869ab42f962955e6abd485d90
NoahOnFyre <noahonfyre@gmail.com>
2023-12-21 19:46:49 +0100

    Correct command descriptions and argument identifiers.

--- registration.go
+++ registration.go
@@ -1,15 +1,15 @@
 package main
 
 func CommandRegistration() {
-	RegisterCommand("flood", "Run a denial of service attack on the target.", []string{"addr", "port"}, FloodCommand)
-	RegisterCommand("portscan", "Scan for open ports on the target.", []string{"addr"}, PortscanCommand)
-	RegisterCommand("gather", "Gather information about the target.", []string{"addr"}, GatherCommand)
+	RegisterCommand("flood", "Run a denial of service attack on the target.", []string{"ip", "port"}, FloodCommand)
+	RegisterCommand("portscan", "Scan for open ports on the target.", []string{"ip"}, PortscanCommand)
+	RegisterCommand("gather", "Gather information about the target.", []string{"ip"}, GatherCommand)
 	RegisterCommand("cd", "Change your current working directory.", []string{"dir"}, CdCommand)
 	RegisterCommand("ls", "List all files in a directory.", []string{}, LsCommand)
-	RegisterCommand("update", "Update your FyUTILS instance.", []string{}, UpdateCommand)
-	RegisterCommand("help", "Show some help about the commands.", []string{}, HelpCommand)
-	RegisterCommand("clear", "Clear the console window.", []string{}, ClearCommand)
-	RegisterCommand("exit", "Exit the application.", []string{}, ExitCommand)
+	RegisterCommand("update", "Update your FyUTILS instance to the newest version.", []string{}, UpdateCommand)
+	RegisterCommand("help", "Show the help about all the commands.", []string{}, HelpCommand)
+	RegisterCommand("clear", "Clear the console screen.", []string{}, ClearCommand)
+	RegisterCommand("exit", "Gracefully exit FyUTILS.", []string{}, ExitCommand)
 }
 
 func RegisterCommand(name string, description string, args []string, runnable func([]string)) {
