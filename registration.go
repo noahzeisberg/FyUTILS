299204cedff43cb8343bfc5d2ea7ddbb862f5b70
NoahOnFyre <noahonfyre@gmail.com>
2023-12-30 17:39:20 +0100

    Register new commands.

--- registration.go
+++ registration.go
@@ -3,7 +3,9 @@ package main
 func CommandRegistration() {
 	RegisterCommand("flood", "Run a denial of service attack on the target.", []string{"ip", "port"}, FloodCommand)
 	RegisterCommand("portscan", "Scan for open ports on the target.", []string{"ip"}, PortscanCommand)
-	RegisterCommand("gather", "Gather information about the target.", []string{"ip"}, GatherCommand)
+	RegisterCommand("whois", "Gather WHOIS information about the target.", []string{"ip"}, WhoisCommand)
+	RegisterCommand("retrieve", "Retrieve local information.", []string{"item"}, RetrieveCommand)
+	RegisterCommand("sniff", "Capture traffic of a specific interface.", []string{"interface"}, SniffCommand)
 	RegisterCommand("cd", "Change your current working directory.", []string{"dir"}, CdCommand)
 	RegisterCommand("ls", "List all files in a directory.", []string{}, LsCommand)
 	RegisterCommand("update", "Update your FyUTILS instance to the newest version.", []string{}, UpdateCommand)
@@ -15,7 +17,7 @@ func CommandRegistration() {
 func RegisterCommand(name string, description string, args []string, runnable func([]string)) {
 	arguments := Args{
 		Count: len(args),
-		Get:   args,
+		Usage: args,
 	}
 	commands = append(commands, Command{
 		Name:        name,
