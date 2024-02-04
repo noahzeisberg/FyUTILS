13278ccd09ccbb98bb37f28948d52e8a356f2968
NoahOnFyre <noahonfyre@gmail.com>
2024-02-04 18:53:04 +0100

    feat: create init.go for initialisation related functions.

---
+++ init.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"github.com/NoahOnFyre/gengine/filesystem"
+	"io/fs"
+	"os"
+)
+
+func CheckUpdates() {
+	release, _, err := githubClient.Repositories.GetLatestRelease(context.Background(), "noahonfyre", "FyUTILS")
+	if err != nil {
+		return
+	}
+	newestRelease = release
+}
+
+func CheckPaths(paths []string) int {
+	pathsFixed := 0
+	for _, path := range paths {
+		if !filesystem.Exists(path) {
+			err := os.Mkdir(path, fs.ModeDir)
+			if err != nil {
+				Error("Failed to create directory!")
+				return pathsFixed
+			}
+			pathsFixed += 1
+		}
+	}
+	return pathsFixed
+}
