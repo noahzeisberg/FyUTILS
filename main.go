6e0ae82b6be3a1d7e900b294b33c6d377e4e737b
NoahOnFyre <noahonfyre@gmail.com>
2024-02-07 17:23:57 +0100

    chore: update version identifier.

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 var (
 	username, _   = strings.CutPrefix(convert.ValueOf(utils.Catch(os.UserHomeDir())), "C:\\Users\\")
 	device, _     = os.Hostname()
-	version       = "v1.19.1"
+	version       = "v1.20.0"
 	homeDir, _    = os.UserHomeDir()
 	mainDir       = homeDir + "\\.fy\\"
 	tempDir       = mainDir + "temp\\"
