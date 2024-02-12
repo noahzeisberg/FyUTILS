bbbcc8bedd8d12fdb561ed396c6bc1fc8ddc7f20
NoahOnFyre <noahonfyre@gmail.com>
2024-02-13 00:16:19 +0100

    feat: export global variables.

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/google/go-github/github"
-	"github.com/noahzeisberg/gengine/color"
-	"github.com/noahzeisberg/gengine/convert"
-	"github.com/noahzeisberg/gengine/utils"
+	"github.com/noahzeisberg/FyUTILS/color"
+	"github.com/noahzeisberg/FyUTILS/log"
 	"golang.org/x/mod/semver"
 	"io"
 	"net/http"
@@ -16,61 +16,60 @@ import (
 )
 
 var (
-	username, _   = strings.CutPrefix(convert.ValueOf(utils.Catch(os.UserHomeDir())), "C:\\Users\\")
-	device, _     = os.Hostname()
-	version       = "v1.22.0"
-	homeDir, _    = os.UserHomeDir()
-	mainDir       = homeDir + "\\.fy\\"
-	tempDir       = mainDir + "temp\\"
-	downloadDir   = mainDir + "download\\"
-	configDir     = mainDir + "config\\"
-	fuelDir       = mainDir + "fuel\\"
-	newestRelease *github.RepositoryRelease
-	httpClient    = &http.Client{Transport: &http.Transport{}}
-	startTime     = time.Now()
-	commands      []Command
+	Username, _   = strings.CutPrefix(fmt.Sprint(Catch(os.UserHomeDir())), "C:\\Users\\")
+	Device, _     = os.Hostname()
+	Version       = "v1.22.0"
+	HomeDir, _    = os.UserHomeDir()
+	MainDir       = HomeDir + "\\.fy\\"
+	TempDir       = MainDir + "temp\\"
+	DownloadDir   = MainDir + "download\\"
+	ConfigDir     = MainDir + "config\\"
+	FuelDir       = MainDir + "fuel\\"
+	NewestRelease *github.RepositoryRelease
+	HttpClient    = &http.Client{Transport: &http.Transport{}}
+	StartTime     = time.Now()
+	Commands      []Command
 )
 
 func main() {
 	go CheckUpdates()
 
 	CheckPaths([]string{
-		homeDir,
-		mainDir,
-		tempDir,
-		downloadDir,
-		configDir,
-		fuelDir,
+		HomeDir,
+		MainDir,
+		TempDir,
+		DownloadDir,
+		ConfigDir,
+		FuelDir,
 	})
 
 	CommandRegistration()
 	Menu()
 
 	for {
-		SetState("Idle")
-		Print()
+		log.Print()
 		currentDir, _ := os.Getwd()
-		input := Input(color.Gray + "┌─[" + color.Blue + username + color.Gray + "@" + color.Reset + device + color.Gray + "]─(" + color.Reset + "\U000F024B" + " " + GetPathAlias(currentDir) + color.Gray + ")\n" + color.Gray + "└─> " + color.Reset)
+		input := log.Input(color.Gray + "┌─[" + color.Blue + Username + color.Gray + "@" + color.Reset + Device + color.Gray + "]─(" + color.Reset + "\U000F024B" + " " + GetPathAlias(currentDir) + color.Gray + ")\n" + color.Gray + "└─> " + color.Reset)
 		if input != "" {
-			Print()
+			log.Print()
 			split := strings.Split(input, " ")
 			command := split[0]
-			args := utils.RemoveElement(split, 0)
+			args := RemoveElement(split, 0)
 			RunCommand(command, args)
 		}
-		if semver.Compare(version, newestRelease.GetTagName()) == -1 {
-			Print()
-			Print(color.Gray + "┌" + MultiString("─", 120-1))
-			Print(color.Gray + "│ " + color.Reset + "A new version of FyUTILS is available! Run " + color.Blue + "\"update\"" + color.Reset + " to download.")
-			Print(color.Gray + "│ " + color.Reset + "Version Diff: " + color.Red + version + color.Gray + " -> " + color.Green + newestRelease.GetTagName())
-			Print(color.Gray + "└" + MultiString("─", 120-1))
+		if semver.Compare(Version, NewestRelease.GetTagName()) == -1 {
+			log.Print()
+			log.Print(color.Gray + "┌" + MultiString("─", 120-1))
+			log.Print(color.Gray + "│ " + color.Reset + "A new version of FyUTILS is available! Run " + color.Blue + "\"update\"" + color.Reset + " to download.")
+			log.Print(color.Gray + "│ " + color.Reset + "Version Diff: " + color.Red + Version + color.Gray + " -> " + color.Green + NewestRelease.GetTagName())
+			log.Print(color.Gray + "└" + MultiString("─", 120-1))
 		}
 	}
 }
 
 func RunCommand(command string, args []string) {
 	commandFound := false
-	for _, cmd := range commands {
+	for _, cmd := range Commands {
 		if cmd.Name == command {
 			var requiredArgs int
 			for _, argument := range cmd.Args {
@@ -79,7 +78,6 @@ func RunCommand(command string, args []string) {
 				}
 			}
 			if len(args) >= requiredArgs {
-				SetState("Running: " + cmd.Name)
 				cmd.Run(args)
 				commandFound = true
 			} else {
@@ -91,7 +89,7 @@ func RunCommand(command string, args []string) {
 						usage += "[" + argument.Identifier + "] "
 					}
 				}
-				Error("Invalid arguments!" + color.Gray + " - " + color.Red + "Usage: " + command + " " + usage)
+				log.Error("Invalid arguments!" + color.Gray + " - " + color.Red + "Usage: " + command + " " + usage)
 				commandFound = true
 			}
 		}
@@ -99,14 +97,13 @@ func RunCommand(command string, args []string) {
 	if !commandFound {
 		_, err := exec.LookPath(command)
 		if err != nil {
-			Error("Command not found! - Run \"help\" to see all commands.")
+			log.Error("Command not found! - Run \"help\" to see all Commands.")
 			return
 		}
 		var cmdArgs []string
 		cmdArgs = append(cmdArgs, "/c")
 		cmdArgs = append(cmdArgs, command)
 		cmdArgs = append(cmdArgs, args...)
-		SetState("Running: " + command)
 		runnable := exec.Command("cmd.exe", cmdArgs...)
 
 		var stdBuffer bytes.Buffer
@@ -117,7 +114,7 @@ func RunCommand(command string, args []string) {
 
 		err = runnable.Start()
 		if err != nil {
-			Error("Failed to run command!", err.Error())
+			log.Error("Failed to run command!", err.Error())
 			return
 		}
 	}
