2e78cec71d5c7934e56058efd86e5743e2e2dc64
NoahOnFyre <noahonfyre@gmail.com>
2024-02-13 00:20:59 +0100

    refactor: move functions from utils into according classes.

--- utils.go
+++ utils.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/rand"
-	"strings"
 )
 
 func RandomBytes(size int) (blk []byte, err error) {
@@ -19,28 +18,6 @@ func MultiString(char string, repeat int) string {
 	return final
 }
 
-func StripPath(path string) string {
-	return strings.TrimSuffix(path, "\\")
-}
-
 func RemoveElement(slice []string, index int) []string {
 	return append(slice[:index], slice[index+1:]...)
-}
-
-func GetPathAlias(path string) string {
-	for _, alias := range pathAliases {
-		if strings.ToLower(alias.Path) == strings.ToLower(path) {
-			return alias.Short
-		}
-	}
-	return path
-}
-
-func GetAliasPath(alias string) string {
-	for _, path := range pathAliases {
-		if path.Short == alias {
-			return path.Path
-		}
-	}
-	return alias
 }
