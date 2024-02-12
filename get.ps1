2eed7cf9654dae465bac94c43ff4fb10c4431eb8
NoahOnFyre <noahonfyre@gmail.com>
2024-02-12 21:04:12 +0100

    fix: reformat `Start-Process` statement.

--- get.ps1
+++ get.ps1
@@ -51,4 +51,4 @@ Write-Output "Installation of FyUTILS complete! Please consider starring this re
 Write-Output "https://github.com/noahonfyre/FyUTILS"
 Write-Output " "
 
-Start-Process "wt.exe fyutils.exe"
+Start-Process wt.exe fyutils.exe
