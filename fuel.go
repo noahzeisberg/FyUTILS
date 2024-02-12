35066f0f61c56f5187156e69b37d2a3ac06330ce
NoahOnFyre <noahonfyre@gmail.com>
2024-02-13 00:21:55 +0100

    refactor: update variable names.

--- fuel.go
+++ fuel.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/go-github/github"
-	"github.com/noahzeisberg/gengine/color"
-	"github.com/noahzeisberg/gengine/networking/requests"
+	"github.com/noahzeisberg/FyUTILS/color"
+	"github.com/noahzeisberg/FyUTILS/log"
+	"github.com/noahzeisberg/FyUTILS/networking/requests"
 	"math"
 	"os"
 	"strings"
@@ -24,20 +25,20 @@ func ParseRepository(repo string) (string, string) {
 func FetchRepositoryContent(pkg string, path string, start time.Time) {
 	owner, repository := ParseRepository(pkg)
 	pkg = owner + "/" + repository
-	packageDirectory := fuelDir + owner + "." + repository + "\\"
+	packageDirectory := FuelDir + owner + "." + repository + "\\"
 
-	Print("Fetching FUEL " + color.Blue + pkg + color.Reset + "...")
+	log.Print("Fetching FUEL " + color.Blue + pkg + color.Reset + "...")
 
 	_, repositoryContents, r, err := githubClient.Repositories.GetContents(context.Background(), owner, repository, path, nil)
 	if err != nil {
-		Error(err.Error())
+		log.Error(err.Error())
 		return
 	}
-	Print("Server returned " + color.Blue + fmt.Sprint(r.StatusCode) + color.Reset + " via " + r.Proto)
+	log.Print("Server returned " + color.Blue + fmt.Sprint(r.StatusCode) + color.Reset + " via " + r.Proto)
 
 	err = os.MkdirAll(packageDirectory+strings.ReplaceAll(path, "/", "\\"), os.ModePerm)
 	if err != nil {
-		Error(err.Error())
+		log.Error(err.Error())
 		return
 	}
 
@@ -47,14 +48,14 @@ func FetchRepositoryContent(pkg string, path string, start time.Time) {
 	for _, props := range repositoryContents {
 		go func(props *github.RepositoryContent) {
 			if props.GetType() != "file" {
-				Print("Not a file! Skipping " + props.GetName() + "...")
+				log.Print("Not a file! Skipping " + props.GetName() + "...")
 				wg.Done()
 				return
 			}
 
 			file, err := os.Create(packageDirectory + props.GetName())
 			if err != nil {
-				Error(err.Error())
+				log.Error(err.Error())
 				return
 			}
 
@@ -62,22 +63,22 @@ func FetchRepositoryContent(pkg string, path string, start time.Time) {
 
 			bytesWritten, err := file.Write(fileContent)
 			if err != nil {
-				Error(err.Error())
+				log.Error(err.Error())
 				return
 			}
 
 			err = file.Close()
 			if err != nil {
-				Error(err.Error())
+				log.Error(err.Error())
 				return
 			}
 
-			Print("File " + color.Blue + props.GetName() + color.Reset + " successfully collected! " + color.Gray + "(" + fmt.Sprint(bytesWritten) + " bytes)")
+			log.Print("File " + color.Blue + props.GetName() + color.Reset + " successfully collected! " + color.Gray + "(" + fmt.Sprint(bytesWritten) + " bytes)")
 
 			wg.Done()
 		}(props)
 	}
 
 	wg.Wait()
-	Print("Successfully collected package " + color.Blue + pkg + color.Reset + "! " + color.Gray + "(" + fmt.Sprint(math.Round(time.Since(start).Seconds())) + "s)")
+	log.Print("Successfully collected package " + color.Blue + pkg + color.Reset + "! " + color.Gray + "(" + fmt.Sprint(math.Round(time.Since(start).Seconds())) + "s)")
 }
