5c155d117e44a1a01df7808ccc52f3b39c24211c
NoahOnFyre <noahonfyre@gmail.com>
2024-05-24 18:43:25 +0200

    refactor: move `Menu()` to formatting.

--- formatting.go
+++ formatting.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/noahzeisberg/FyUTILS/color"
+	"github.com/noahzeisberg/FyUTILS/log"
 	"github.com/noahzeisberg/FyUTILS/utils"
 )
 
@@ -30,4 +31,14 @@ func GroupContainer(contents ...Group) string {
 	}
 	container += color.Gray + "└" + utils.MultiString("─", 120-1)
 	return container
+}
+
+func Menu() {
+	log.Print(color.Blue + "    ______      __  ______________   _____")
+	log.Print(color.Blue + "   / ____/_  __/ / / /_  __/  _/ /  / ___/")
+	log.Print(color.Blue + "  / /_  / / / / / / / / /  / // /   \\__ \\" + "     " + color.Reset + " Version" + ": " + color.Blue + Version + color.Reset)
+	log.Print(color.Blue + " / __/ / /_/ / /_/ / / / _/ // /______/ /" + "     " + color.Reset + " User" + ": " + color.Blue + Username + color.Reset)
+	log.Print(color.Blue + "/_/    \\__, /\\____/ /_/ /___/_____/____/" + "      " + color.Reset + " Device" + ": " + color.Blue + Device + color.Reset)
+	log.Print(color.Blue + "       __/ /")
+	log.Print(color.Blue + "     /____/")
 }
