5ad7e72d0c627b6c0c8499306a3f32a6dc30e8c0
NoahOnFyre <noahonfyre@gmail.com>
2024-05-27 14:52:49 +0200

    feat: update main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fyutils/log"
+	"fyutils/parser"
+)
 
+func main() {
+	for {
+		input := log.Input("Enter command: ")
+		cmdName, args := parser.ParseInput(input)
+		command, err := parser.GetCommand(cmdName)
+		if err != nil {
+			log.Error(err)
+		}
+		command.Run(&command, args)
+	}
 }
