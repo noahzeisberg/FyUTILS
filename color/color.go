cace704e524f2371cc60ee980f8465380c8a540e
NoahOnFyre <noahonfyre@gmail.com>
2024-05-27 14:25:47 +0200

    feat: add color package.

---
+++ color/color.go
@@ -0,0 +1,23 @@
+package color
+
+var (
+	Reset     = "\033[0m"
+	Black     = "\033[30m"
+	BlackBg   = "\033[40m"
+	Red       = "\033[31m"
+	RedBg     = "\033[41m"
+	Green     = "\033[32m"
+	GreenBg   = "\033[42m"
+	Yellow    = "\033[33m"
+	YellowBg  = "\033[43m"
+	Blue      = "\033[34m"
+	BlueBg    = "\033[44m"
+	Magenta   = "\033[35m"
+	MagentaBg = "\033[45m"
+	Cyan      = "\033[36m"
+	CyanBg    = "\033[46m"
+	White     = "\033[37m"
+	WhiteBg   = "\033[47m"
+	Gray      = "\033[90m"
+	GrayBg    = "\033[100m"
+)
