06225c16f5663c132d70637539155d0fe7cd35dd
NoahOnFyre <noahonfyre@gmail.com>
2024-06-02 19:53:28 +0200

    feat: add container and group container.

--- log/logging.go
+++ log/logging.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"fyutils/color"
+	"fyutils/types"
+	"fyutils/utils"
 	"os"
 	"strings"
 )
@@ -50,4 +52,30 @@ func Confirm(title string) bool {
 
 func Clear() {
 	PrintR("\033[H\033[2J")
+}
+
+func Container(rows ...string) string {
+	var container string
+	container += color.Gray + "┌" + utils.MultiString("─", 120-1) + "\n"
+	for _, row := range rows {
+		container += color.Gray + "│ " + color.Reset + row + "\n"
+	}
+	container += color.Gray + "└" + utils.MultiString("─", 120-1)
+	return container
+}
+
+func GroupContainer(contents ...types.Group) string {
+	var maxLength int
+	for _, item := range contents {
+		if len(fmt.Sprint(item.A)) > maxLength {
+			maxLength = len(fmt.Sprint(item.A))
+		}
+	}
+	var container string
+	container += color.Gray + "┌" + utils.MultiString("─", 120-1) + "\n"
+	for _, item := range contents {
+		container += color.Gray + "│ " + color.Blue + fmt.Sprint(item.A) + utils.MultiString(" ", maxLength-len(fmt.Sprint(item.A))+3) + color.Reset + fmt.Sprint(item.B) + "\n"
+	}
+	container += color.Gray + "└" + utils.MultiString("─", 120-1)
+	return container
 }
