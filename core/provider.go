2d3fae0e5a41aee504a4b64cda0f2791a1bafa48
NoahOnFyre <noahonfyre@gmail.com>
2024-06-06 15:38:39 +0200

    feat: add more data

--- core/provider.go
+++ core/provider.go
@@ -9,10 +9,12 @@ import (
 )
 
 var (
-	User, _      = user.Current()
-	Device, _    = os.Hostname()
-	Home, _      = os.UserHomeDir()
-	HttpClient   = http.Client{Transport: &http.Transport{}}
-	GithubClient = github.NewClient(&HttpClient)
-	Commands     []types.Command
+	User, _       = user.Current()
+	Device, _     = os.Hostname()
+	Home, _       = os.UserHomeDir()
+	Version       = "v1.23.0"
+	HttpClient    = http.Client{Transport: &http.Transport{}}
+	GithubClient  = github.NewClient(&HttpClient)
+	Commands      []types.Command
+	LatestRelease *github.RepositoryRelease
 )
