f209c8fd9eadbacc3a2a28330cccaea8a9ad0417
NoahOnFyre <noahonfyre@gmail.com>
2024-06-06 16:30:05 +0200

    feat: add directory variables.

--- core/provider.go
+++ core/provider.go
@@ -13,6 +13,10 @@ var (
 	Device, _     = os.Hostname()
 	HomeDir, _    = os.UserHomeDir()
 	MainDir       = HomeDir + "\\.fy\\"
+	ExtensionsDir = MainDir + "\\extensions\\"
+	TempDir       = MainDir + "\\temp\\"
+	LogsDir       = MainDir + "\\logs\\"
+	DownloadDir   = MainDir + "\\download\\"
 	Version       = "v1.23.0"
 	HttpClient    = http.Client{Transport: &http.Transport{}}
 	GithubClient  = github.NewClient(&HttpClient)
