56d283a94cf9e6e9e39c6493090fadc13c0cc748
NoahOnFyre <noahonfyre@gmail.com>
2024-06-06 15:39:21 +0200

    fix: update checker

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fyutils/color"
 	"fyutils/core"
 	"fyutils/log"
+	"golang.org/x/mod/semver"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -20,9 +23,9 @@ func main() {
 
 	core.RegisterCommands()
 	go core.InitializeReleaseSubscriber()
-
 	for {
-		input := log.Input(color.Gray + "┌──(" + color.Blue + "noah" + color.Gray + "@" + color.Reset + "DESKTOP" + color.Gray + ")─[" + color.Reset + "~" + color.Gray + "]\n└─> " + color.Reset)
+		workingDir, _ := os.Getwd()
+		input := log.Input(color.Gray + "┌──(" + color.Blue + strings.Split(core.User.Username, "\\")[1] + color.Gray + "@" + color.Reset + core.Device + color.Gray + ")─[" + color.Reset + workingDir + color.Gray + "]\n└─> " + color.Reset)
 		cmdName, args := core.ParseInput(input)
 		command, err := core.GetCommand(cmdName)
 		if err != nil {
@@ -30,10 +33,16 @@ func main() {
 			continue
 		}
 
+		log.Print()
 		err = command.Run(args)
 		if err != nil {
 			log.Error(cases.Title(language.English, cases.Compact).String(err.Error()) + ".")
-			return
+			continue
+		}
+		log.Print()
+
+		if semver.Compare(core.LatestRelease.GetTagName(), core.Version) == 1 {
+			log.Print(log.Container("A new FyUTILS release is freshly available for you!", "Diff: "+color.Red+core.Version+color.Gray+" -> "+color.Green+core.LatestRelease.GetTagName()))
 		}
 	}
 }
