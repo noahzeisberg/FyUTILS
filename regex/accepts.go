98cab0f91ed5f38975c175dca2fc6b7d8177f144
NoahOnFyre <noahonfyre@gmail.com>
2024-07-13 23:26:41 +0200

    fix: remove unsafe and temporary functions.

--- regex/accepts.go
+++
@@ -1,9 +0,0 @@
-package regex
-
-import "regexp"
-
-var (
-	AcceptsIP   = regexp.MustCompile("^(1-255).(1-255).(1-255).(1-255)$")
-	AcceptsPort = regexp.MustCompile("^(1-65536)$")
-	AcceptsAny  = regexp.MustCompile("^*$")
-)
