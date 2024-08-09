7882564a4331a46e6ae4b5b4d50c96fb6d544c87
NoahOnFyre <noahonfyre@gmail.com>
2024-08-09 17:55:50 +0200

    feat: add test code for outputting argumments and logging an error.

--- cli/init.go
+++ cli/init.go
@@ -1,9 +1,16 @@
 package cli
 
+import (
+	"github.com/noahzeisberg/FyUTILS/cli/log"
+	"net/http"
+)
+
 var (
 	Version string = "v2.0.0"
 )
 
 func Main(args []string) {
-
+	log.Println(args)
+	_, err := http.Get("https://asigansiiagsidnasndaw.tk")
+	log.Error(err, true)
 }
